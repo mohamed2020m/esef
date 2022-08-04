87f9474559f6841b3c480bb16d2cffb9a46697ec
Hmayda Abde ssamad <abdoessamadhmayda@gmail.com>
2022-08-04 21:41:27 +0100

    Gestion utilisaturs2

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use PhpParser\Node\Stmt\Global_;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
