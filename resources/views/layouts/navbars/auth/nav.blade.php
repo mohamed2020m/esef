020342ab36a5eba942547bc030fda97a74f91663
leeuw <mohamedessabir20@gmail.com>
2022-08-04 22:12:36 +0100

    fixing the accents typo

--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -3,7 +3,7 @@
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                 <!-- <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li> -->
-                <li class="breadcrumb-item text-dark active text-capitalize" aria-current="page">html_entity_decode({{ str_replace('-', ' ', Request::path()) }}) /</li>
+                <li class="breadcrumb-item text-dark active text-capitalize" aria-current="page"><?php html_entity_decode("{{ str_replace('-', ' ', Request::path()) }}") ?> /</li>
             </ol>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex justify-content-between" id="nav-header"> 
