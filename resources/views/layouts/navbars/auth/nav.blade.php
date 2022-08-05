b52d27c3ef766c38ae5b186f9502926101f17a65
elbahjacharafeddine <elbahjacharafeddine2020@gmail.com>
2022-08-05 23:39:20 +0100

    5 elbahja commit

--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -3,7 +3,7 @@
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                 <!-- <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li> -->
-                <li class="breadcrumb-item text-dark active text-capitalize" aria-current="page"><?php echo imap_utf8(str_replace('-', ' ', Request::path())) ?> /</li>
+                <li class="breadcrumb-item text-dark active text-capitalize" aria-current="page"><?php echo "user/profile"; ?> /</li>
 
             </ol>
         </nav>
