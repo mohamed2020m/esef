e896220e9abd1e42c2732a5a9466afc6ae63172a
leeuw <mohamedessabir20@gmail.com>
2022-08-05 04:26:35 +0100

    DashBorad:: design 25

--- resources/views/statistique.blade.php
+++ resources/views/statistique.blade.php
@@ -566,7 +566,7 @@
             tension: 0.4,
             borderWidth: 0,
             pointRadius: 0,
-            borderColor: "#cb0c9f",
+            borderColor: "#0f233a",
             borderWidth: 3,
             backgroundColor: gradientStroke1,
             fill: true,
