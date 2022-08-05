3ca683bf8ca5081509c9e226e3635b059beacf15
leeuw <mohamedessabir20@gmail.com>
2022-08-05 01:32:21 +0100

    DashBorad:: design 3

--- resources/views/statistique.blade.php
+++ resources/views/statistique.blade.php
@@ -13,7 +13,6 @@
                 <p class="text-sm mb-0 text-capitalize font-weight-bold">le nombre de filière</p>
                 <h5 class="font-weight-bolder mb-0">
                     8
-                  <span class="text-success text-sm font-weight-bolder">+10</span>
                 </h5>
               </div>
             </div>
@@ -29,17 +28,16 @@
     <div class="mb-xl-0 mb-4">
       <div class="card">
         <div class="card-body p-3">
-          <div class="row">
-            <div class="col-8">
+          <div class="d-fle">
+            <div class="mx-3">
               <div class="numbers">
                 <p class="text-sm mb-0 text-capitalize font-weight-bold">le nombre des candidats</p>
                 <h5 class="font-weight-bolder mb-0">
                     250
-                  <span class="text-success text-sm font-weight-bolder">+30</span>
                 </h5>
               </div>
             </div>
-            <div class="col-4 text-end">
+            <div class="mx-3 text-end">
               <div class="icon icon-shape text-center border-radius-md text-white" style="background-color:#0f233a">
                 <i class="fa fa-users text-lg opacity-10" aria-hidden="true"></i>
               </div>
@@ -51,7 +49,7 @@
   </div>
   
   <div class="row mt-4">
-    <div class="col-lg-5 mb-lg-0 mb-4">
+    <!-- <div class="col-lg-5 mb-lg-0 mb-4">
       <div class="card z-index-2">
         <div class="card-body p-3">
           <div class="bg-gradient-dark border-radius-lg py-3 pe-1 mb-3">
@@ -166,7 +164,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
     <div class="col-lg-7">
       <div class="card z-index-2">
         <div class="card-header pb-0">
