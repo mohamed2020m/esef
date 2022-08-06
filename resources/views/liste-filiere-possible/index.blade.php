03b2ccefb313f53ba0bf4e5b4800bd96f40d241e
elbahjacharafeddine <elbahjacharafeddine2020@gmail.com>
2022-08-06 12:39:30 +0100

    8 elbahja commit

--- resources/views/liste-filiere-possible/index.blade.php
+++ resources/views/liste-filiere-possible/index.blade.php
@@ -136,28 +136,25 @@
    aria-hidden="true">
    <div class="modal-dialog modal-notify modal-success" role="document">
      <!--Content-->
-     <div class="modal-content">
-       <!--Header-->
-       <div class="modal-header">
-         <p class="heading lead">ESEF</p>
-
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-           <span aria-hidden="true" class="white-text">&times;</span>
-         </button>
+        <div class="modal-content">
+            <button type="button" class="btn btn-secondary rounded-circle px-2 py-1" data-bs-dismiss="modal" aria-label="Close">
+                <i class="fa fa-close"></i>
+            </button>
        </div>
 
        <!--Body-->
        <div class="modal-body">
          <div class="text-center">
-           <i class="fas fa-check fa-4x mb-3 animated rotateIn"></i>
+           <!-- <i class="fas fa-check fa-4x mb-3 animated rotateIn"></i> -->
+           <i class="fas fa-exclamation-circle"></i>
            <p>Veuillez remplir vos informations personnelles.</p>
          </div>
        </div>
 
        <!--Footer-->
-       <div class="modal-footer justify-content-center">
+       <!-- <div class="modal-footer justify-content-center">
         <button class="btn btn-success">OK</button>
-       </div>
+       </div> -->
      </div>
      <!--/.Content-->
    </div>
