8b3ddb273e3f0e1d796ae36028a7279bb0e19a3e
leeuw <mohamedessabir20@gmail.com>
2022-08-07 06:07:47 +0100

    update 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,10 @@
   <link rel="stylesheet" href="../assets/css/custom-css.css">
   <!-- Datatable CSS -->
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css"/>
+    <!-- jQuery Library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <!-- Datatable JS -->
+  <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 </head>
 
 <body class="g-sidenav-show  bg-gray-100 ">
@@ -68,10 +72,7 @@
       <p class="m-0">{{ session('success')}}</p>
     </div>
   @endif
-  <!-- jQuery Library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <!-- Datatable JS -->
-  <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+
   <!--   Core JS Files   -->
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="../assets/js/core/popper.min.js"></script>
