3a9a2bc141c5b18274598e1a98ddd49a29e3bd6f
elbahjacharafeddine <elbahjacharafeddine2020@gmail.com>
2022-08-07 19:01:05 +0100

    elbahja commit

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -49,7 +49,7 @@
         <li class="nav-item">
           <a class="nav-link {{ (Request::is('change-password') ? 'active' : '') }}" href="{{ url('change-password') }}">
             <div class="icon-btn icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i style="font-size: 1rem;" class="fa fa-lg fa-lock ps-2 pe-2 text-center text-dark {{ (Request::is('change-password') ? 'text-white' : 'text-dark') }} " aria-hidden="true"></i>
+              <i style="font-size: 1rem;" class="fa fa-lg fa-lock ps-2 pe-2 text-center text-dark {{ (Request::is('change-password') ? 'bg-dark' : 'bg-white') }} " aria-hidden="true"></i>
             </div>
             <span class="nav-link-text ms-1">Changer mot de passe</span>
           </a>
