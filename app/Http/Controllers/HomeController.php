dd4f0bf259caebcbf9224463e6e7c066f093ae23
Hmayda Abde ssamad <abdoessamadhmayda@gmail.com>
2022-08-08 00:34:20 +0100

    to_essabir

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,25 +35,8 @@ class HomeController extends Controller
                 $data=DB::table('users')->select('*')->join('filiere_user','filiere_user.user_id','=','users.id')
                 ->join('filieres','filieres.id','=','filiere_user.filiere_id')->where('filieres.id',$request->id)->get();
 
-                foreach($data as $candidat){
-
-                    $matieres=DB::table('matiere_user')->select('matiere_user.*')->join('matieres','matieres.id','=','matiere_user.matiere_id')
-                    ->where('matiere_user.user_id',$candidat->user_id)->get();
-                    
-
-
-                  foreach($matieres as $matiere){
-
-                    echo(gettypet($matiere));
-
-                  }
-
-
-                }
-
-                
-                 
-            return response()->json($data);
+                return response()->json($data);
+              
 
                       }else{
 
