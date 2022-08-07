ee1bc021fe9c63792ab170c195958bdc63b8345f
elbahjacharafeddine <elbahjacharafeddine2020@gmail.com>
2022-08-07 02:12:30 +0100

    elbahja commit

--- resources/views/layouts/navbars/auth/sidebar.blade-old2.php
+++ resources/views/layouts/navbars/auth/sidebar.blade-old2.php
@@ -29,6 +29,17 @@
           </a>
         </li>
         <li class="nav-item">
+          <a class="nav-link {{ (Request::is('change-password') ? 'active' : '') }}" href="{{ url('change-password') }}">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                <i style="font-size: 1rem;" class="fa fa-lg fa-lock ps-2 pe-2 text-center text-dark {{ (Request::is('change-password') ? 'text-white' : 'text-dark') }} " aria-hidden="true"></i>
+            </div>
+            <span class="nav-link-text ms-1">Changer mot de passe</span>
+          </a>
+        </li>
+
+
+        <i class="fa fa-lock" aria-hidden="true"></i>
+        <li class="nav-item">
           <a class="nav-link {{ (Request::is('logout') ? 'active' : '') }}" href="{{ url('logout') }}">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <i style="font-size: 1rem;" class="fa fa-lg fa-sign-out ps-2 pe-2 text-center text-dark {{ (Request::is('logout') ? 'text-white' : 'text-dark') }} " aria-hidden="true"></i>
