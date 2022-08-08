fb144a5e02733b5e87e6ecbccae3ba947af3dd2f
leeuw <mohamedessabir20@gmail.com>
2022-08-08 15:48:53 +0100

    dashboard 8/8/22 :: 7

--- resources/views/statistique.blade.php
+++ resources/views/statistique.blade.php
@@ -60,7 +60,7 @@
               <canvas id="chart-bars" class="chart-canvas" height="300"></canvas>
             </div>
           </div> -->
-          <div id="chartCondidate" style="height: 500px; width: 100%;"></div>
+          <div id="chartCondidate" style="height: 300px; width: 100%;"></div>
         </div>
       </div>
     </div>
@@ -485,7 +485,7 @@ var chart = new CanvasJS.Chart("chartCondidate", {
 		text: "Nombre des candidats par rapport au nombre des filières"
 	},
 	axisY: {
-		title: "Candidats"
+		title: "Nombre de candidatures"
 		// suffix: "%"
 	},
 	axisX: {
