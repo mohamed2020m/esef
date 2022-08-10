c0ece88e736a4503bf14e4463cf6d06440439794
leeuw <mohamedessabir20@gmail.com>
2022-08-10 07:46:04 +0100

    removing the icon from export btn

--- resources/views/laravel-examples/user-management.blade.php
+++ resources/views/laravel-examples/user-management.blade.php
@@ -121,7 +121,6 @@
                                     <button type="button" class="btn btn-secondary mt-3 rounded-pill" data-bs-dismiss="modal">Annuler</button>
                                     <a href="/server.php/condidatures-export/${filiere_id}" id="export_btn" class="bg-success rounded-pill px-3 py-2" data-bs-toggle="tooltip" data-bs-original-title="supprimer Bac"> 
                                         <!-- onclick="return confirm('est ce que vous etes sur ?')"> -->
-                                        <i class="cursor-pointer fa fa-file-excel text-white" style="font-weight:normal"></i>
                                         <span class="d-sm-inline d-none text-white" id="span_export">Export</span>
                                     </a>
                                 </div>
