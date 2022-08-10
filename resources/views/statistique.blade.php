3ff8ea67b4402bc7eb522c51c19a4c465f092f3f
leeuw <mohamedessabir20@gmail.com>
2022-08-10 21:00:26 +0100

    dashboard - 8/10/22 :: 4

--- resources/views/statistique.blade.php
+++ resources/views/statistique.blade.php
@@ -557,6 +557,22 @@ chart_2.render();
           // borderSkipped: false,
           backgroundColor: "#fff",
           data: [52, 19, 30, 50, 20, 23],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)'
+          ],
           // maxBarThickness: 6
         }, ],
       },
@@ -591,7 +607,7 @@ chart_2.render();
                 style: 'normal',
                 // lineHeight: 2
               },
-              // color: "#fff"
+              color: "#fff"
             },
           },
           x: {
