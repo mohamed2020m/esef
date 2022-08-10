cc929ce3f2b7dbfbc46458436c55538a222a7622
leeuw <mohamedessabir20@gmail.com>
2022-08-10 23:34:31 +0100

    dashboard - 8/10/22 :: 45

--- resources/views/statistique.blade.php
+++ resources/views/statistique.blade.php
@@ -73,21 +73,19 @@
                   @foreach($data_filiere as $row)
                   <option value="{{$row->id}}">{{$row->name}}</option>
                   @endforeach --}}
-                  <select>
-                    <option disabled selected>Sélectionnez un mois</option>
-                    <option value="1">Janvier</option>
-                    <option value="2">Février</option>
-                    <option value="3">Mars</option>
-                    <option value="4">Avril</option>
-                    <option value="5">Mai</option>
-                    <option value="6">Juin</option>
-                    <option value="7">Juillet</option>
-                    <option value="8">Août</option>
-                    <option value="9">Septembre</option>
-                    <option value="10">Octobre</option>
-                    <option value="11">Novembre</option>
-                    <option value="12">Décembre</option>
-                  </select>
+                  <option disabled selected>Sélectionnez un mois</option>
+                  <option value="1">Janvier</option>
+                  <option value="2">Février</option>
+                  <option value="3">Mars</option>
+                  <option value="4">Avril</option>
+                  <option value="5">Mai</option>
+                  <option value="6">Juin</option>
+                  <option value="7">Juillet</option>
+                  <option value="8">Août</option>
+                  <option value="9">Septembre</option>
+                  <option value="10">Octobre</option>
+                  <option value="11">Novembre</option>
+                  <option value="12">Décembre</option>
               </select>
           </form>
           </div>
