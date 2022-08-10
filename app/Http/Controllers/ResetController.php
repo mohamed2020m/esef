d38f46fd09a28771912596eb38b83884f44ed03b
elbahjacharafeddine <elbahjacharafeddine2020@gmail.com>
2022-08-10 22:19:03 +0100

    elbahja commit

--- app/Http/Controllers/ResetController.php
+++ app/Http/Controllers/ResetController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\View;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class ResetController extends Controller
