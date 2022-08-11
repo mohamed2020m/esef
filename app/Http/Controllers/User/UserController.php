cfc5d3f1b248c355ee5ff9851e25f21b2a9d49db
Hmayda Abde ssamad <abdoessamadhmayda@gmail.com>
2022-08-11 12:23:23 +0100

    hy

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -25,11 +25,11 @@ class UserController extends Controller
         }
     }
 
-    public function state(Request $request){
+    public function state($id){
         if(Auth::user()->role =="admin"){
-          
+            //User::find($id)->update
           $state_value=DB::table('users')->select('users.*')->where('users.id',$request->id)->get();
-          return response()->json($state_value->values()->all());
+          return response()->json(['success'=>'User Updated Successfully!']);
         }
         else{
             return  redirect('dashboard');
