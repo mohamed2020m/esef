5f18b5a56754b785f84ccef05136640f455ca2c1
Hmayda Abde ssamad <abdoessamadhmayda@gmail.com>
2022-08-11 21:16:24 +0100

    h

--- resources/views/second-view/candidat/liste_des_inscrits.blade.php
+++ resources/views/second-view/candidat/liste_des_inscrits.blade.php
@@ -43,6 +43,27 @@
         </div>
     </div>
 
+    <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <script>
 
@@ -77,7 +98,7 @@
                                 "render": function ( data, type, row) {
 
                                     if ( data == "0"){
-                                        return ' <button type="button" class="btn btn-success">Activer</button>';
+                                        return ' <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">Activer</button>';
                                     }else{
                                         return '<button type="button" class="btn btn-danger">Désactiver</button>';
                                     }
