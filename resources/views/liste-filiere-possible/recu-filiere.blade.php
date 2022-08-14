e631ca420a7f6b471dafcbcf1ea16fd41d67f388
leeuw <mohamedessabir20@gmail.com>
2022-08-14 05:00:46 +0100

    General fixing :: 9

--- resources/views/liste-filiere-possible/recu-filiere.blade.php
+++ resources/views/liste-filiere-possible/recu-filiere.blade.php
@@ -14,28 +14,6 @@
         body{
             font-family: Arial, Helvetica, sans-serif;
         }
-        /* header p {
-            position: relative;
-            margin-top: -90px;
-            margin-left: -40px;
-            text-align: center;
-        } */
-        #fin {
-            position: relative;
-            margin-left: 400px;
-            margin-top: -90px;
-            width: 320px;
-            height: 90px;
-        }
-
-        #titre {
-            position: relative;
-            /* margin-left: 140px; */
-            margin-top: 40px;
-            width: 500px;
-            height: 40px;
-            text-align: center;
-        }
         #titre p{
             font-size: 26px;
         }
@@ -44,38 +22,21 @@
             text-align: center;
             margin-top: 50px;
         }
-
-        /* #attestation {
-            position: relative;
-            margin-left: 60px;
-            margin-top:50px;
-        } */
-
-        /* #signature {
-            position: relative;
-            margin-left: 350px;
-        } */
-        /* footer {
-            text-align: center;
-            font-style: oblique;
-            font-size: large;
-        } */
     </style>
 </head>
 
 <body>
     <div>
         <img src="../assets/img/esef.png"  style="max-width:100%" alt="Logo de Université Chouaib Doukkali">
-        {{-- <img id="fin" src="{{public_path('img/image_recu/universite.jpg')}}" > --}}
     </div>
     
     <div class="container">
-        <div id="titre">
-            <p style="text-decoration: underline">Fiche de pré-candidature </p>
+        <div id="titre" style="display: flex; justify-content:center;margin-top: 40px;">
+            <p style="text-decoration: underline; text-align:center">Fiche de pré-candidature </p>
         </div>
         <div id="attestation">
             <div>
-                <div style="display:flex; flex-direction:column;justify-content-center;border:1px solid rgb(157, 157, 157); padding:10px">
+                <div style="display:flex; flex-direction:column;justify-content:center;border:1px solid rgb(157, 157, 157); padding:10px">
                     @foreach ($user_data as $item)
                     <p id="paratest"><span style="width:100px">Nom </span>:<strong style="text-transform: uppercase;margin-top:30px">{{$item->last_name}} </strong> </p>
                     <p id="paratest"><span style="width:100px">Prénom</span>:<strong style="text-transform: uppercase;">{{$item->first_name}}</strong> </p>
@@ -94,7 +55,7 @@
             </div>
         </div>
     </div>
-    <footer style="position:fixed;bottom:0; margin-top:10px">
+    <footer style="position:fixed;bottom:0;">
         <hr>
         <div style="font-size: 10; display:flex;flex-wrap: wrap;">Route Nationale N°1 (Route AZEMMOUR), Km6, HAOUZIA BP:5096 ElJadida Plateau 24002</div>
         <div style="font-size: 10; display:flex;flex-wrap: wrap;font-weight:bold"> Téléphone: 0523 39 56 79-0523 34 48 22 /fax : 0523 39 49 15</div>
