22520ad3e33e28f90647f0d17afb30e2862599f6
leeuw <mohamedessabir20@gmail.com>
2022-08-14 10:19:11 +0100

    Chart 2 :: fixing - 25

--- resources/views/statistique.blade.php
+++ resources/views/statistique.blade.php
@@ -64,25 +64,22 @@
       <div class="card z-index-2">
         <div class="card-body p-3">
           <div>
-            <form action="" method="">
-              @csrf
-              <select class="form-select form-select-lg" id="month"
-                  style="border-color:#0f233a !important; box-shadow:none !important" aria-label="Default select example" name="mois" required>
-                  <option disabled selected>Le mois En Cours</option>
-                  <option value="1">Janvier</option>
-                  <option value="2">Février</option>
-                  <option value="3">Mars</option>
-                  <option value="4">Avril</option>
-                  <option value="5">Mai</option>
-                  <option value="6">Juin</option>
-                  <option value="7">Juillet</option>
-                  <option value="8">Août</option>
-                  <option value="9">Septembre</option>
-                  <option value="10">Octobre</option>
-                  <option value="11">Novembre</option>
-                  <option value="12">Décembre</option>
-              </select>
-          </form>
+            <select class="form-select form-select-lg" id="month"
+                style="border-color:#0f233a !important; box-shadow:none !important" aria-label="Default select example" name="mois" required>
+                <option disabled selected>Le mois En Cours</option>
+                <option value="1">Janvier</option>
+                <option value="2">Février</option>
+                <option value="3">Mars</option>
+                <option value="4">Avril</option>
+                <option value="5">Mai</option>
+                <option value="6">Juin</option>
+                <option value="7">Juillet</option>
+                <option value="8">Août</option>
+                <option value="9">Septembre</option>
+                <option value="10">Octobre</option>
+                <option value="11">Novembre</option>
+                <option value="12">Décembre</option>
+            </select>
           </div>
           <div class="border-radius-lg py-3 pe-1 mb-3">
             <div class="chart" id="chart_line">
