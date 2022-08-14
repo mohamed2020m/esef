a28757f9d033c5470f667cbd61681d014dcdde0c
leeuw <mohamedessabir20@gmail.com>
2022-08-14 07:16:12 +0100

    Chart 1 :: fixing - 11

--- resources/views/statistique.blade.php
+++ resources/views/statistique.blade.php
@@ -180,7 +180,9 @@
               beginAtZero: true,
               padding: 15,
               // display: true
-              text:'Filières'
+              title:{
+                text:'Filières'
+              }
               font: {
                 size: 14,
                 family: "Open Sans",
@@ -199,7 +201,9 @@
             },
             ticks: {
               // display: true
-              text:'Nombre des candidats'
+              title:{
+                text:'Nombre des candidats'
+              }
             },
           },
         },
