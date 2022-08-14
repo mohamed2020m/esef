97f900e9e06fd1bcd723e95c66dd4b01f0046045
leeuw <mohamedessabir20@gmail.com>
2022-08-14 05:26:25 +0100

    General fixing :: 10

--- resources/views/liste-filiere-possible/recu-filiere.blade.php
+++ resources/views/liste-filiere-possible/recu-filiere.blade.php
@@ -26,8 +26,13 @@
 </head>
 
 <body>
-    <div>
-        <img src="../assets/img/esef.png"  style="max-width:100%" alt="Logo de Université Chouaib Doukkali">
+    <div style="display:flex;">
+        <div>
+            <img src="../assets/img/newLogo.png" style="max-width:100%" alt="ESEF Logo">
+        </div>
+        <div>
+            <img src="../assets/img/esef.png"  style="max-width:100%" alt="Logo de Université Chouaib Doukkali">
+        </div>
     </div>
     
     <div class="container">
@@ -38,28 +43,28 @@
             <div>
                 <div style="display:flex; flex-direction:column;justify-content:center;border:1px solid rgb(157, 157, 157); padding:10px">
                     @foreach ($user_data as $item)
-                    <p id="paratest"><span style="width:100px">Nom </span>:<strong style="text-transform: uppercase;margin-top:30px">{{$item->last_name}} </strong> </p>
-                    <p id="paratest"><span style="width:100px">Prénom</span>:<strong style="text-transform: uppercase;">{{$item->first_name}}</strong> </p>
-                    <p id="paratest"><span style="width:100px">CIN </span>:<strong> {{$item->cin}}</strong></p>
-                    <p id="paratest"><span style="width:100px">Email </span>:<strong> {{$item->email}}</strong></p>
-                    <p id="paratest"><span style="width:100px">Téléphone </span>:<strong> {{$item->phone}}</strong></p>
+                    <p><span style="width:300px">Nom </span>:<strong style="text-transform: uppercase;margin-top:30px">{{$item->last_name}} </strong> </p>
+                    <p><span style="width:100px">Prénom</span>:<strong style="text-transform: uppercase;">{{$item->first_name}}</strong> </p>
+                    <p><span style="width:100px">CIN </span>:<strong> {{$item->cin}}</strong></p>
+                    <p><span style="width:100px">Email </span>:<strong> {{$item->email}}</strong></p>
+                    <p><span style="width:100px">Téléphone </span>:<strong> {{$item->phone}}</strong></p>
                     @endforeach
                 </div>
                 
                 <div>
-                    <p id="post_condidat" style="text-decoration: underline">CANDIDATURE à la FILIERE</p>
-                    <p id="paratest">Etablissement :<strong> Ecole Supérieure d'Education et de Formation (ESEF) - EL JADIDA</strong></p>
-                    <p id="paratest">Spécialité :<strong>{{$filiere_data[0]->name}}</strong></p>
-                    <p id="signature">Reçu LE : {{$register_in_filiere_at[0]->date}}</p>
+                    <p id="post_condidat" style="text-decoration: underline">CANDIDATURE à la FILIÈRE</p>
+                    <p>Etablissement :<strong> Ecole Supérieure d'Education et de Formation (ESEF) - EL JADIDA</strong></p>
+                    <p>Spécialité :<strong>{{$filiere_data[0]->name}}</strong></p>
+                    <p>Reçu LE : <strong>{{$register_in_filiere_at[0]->date}}</strong></p>
                 </div>
             </div>
         </div>
     </div>
-    <footer style="position:fixed;bottom:0;">
+    <div style="position:fixed;bottom:0;">
         <hr>
         <div style="font-size: 10; display:flex;flex-wrap: wrap;">Route Nationale N°1 (Route AZEMMOUR), Km6, HAOUZIA BP:5096 ElJadida Plateau 24002</div>
         <div style="font-size: 10; display:flex;flex-wrap: wrap;font-weight:bold"> Téléphone: 0523 39 56 79-0523 34 48 22 /fax : 0523 39 49 15</div>
-    </footer>
+    </div>
 </body>
 </html>
 
