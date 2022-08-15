d32195035b36fe9b3ee12a2fbea2cfdaee287af1
elbahjacharafeddine <elbahjacharafeddine2020@gmail.com>
2022-08-16 00:12:44 +0100

    elbahja commit

--- app/Http/Controllers/InfoUserController.php
+++ app/Http/Controllers/InfoUserController.php
@@ -75,11 +75,11 @@ class InfoUserController extends Controller
             $image  = time().'.'.$request->image_profile->extension();
             $request->image_profile->move(public_path('images/images_profiles'), $image);
         }
-        else if(Auth::user()->photo !="student_avatar.jpg"){
+        else if(Auth::user()->photo !="student_avatar.png"){
             $image = Auth::user()->photo;
         }
         else{
-            $image = "student_avatar.jpg";
+            $image = "student_avatar.png";
         }
 
         if($request->cin_first_face !=""){
